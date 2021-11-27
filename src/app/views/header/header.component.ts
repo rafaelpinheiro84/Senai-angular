628a35fccdbe6b7fc708e734580d672035b218f7
rafaelpinheiro84 <rafaelsp0584@gmail.com>
2021-11-26 21:24:38 -0300

    modificado

--- src/app/views/header/header.component.ts
+++ src/app/views/header/header.component.ts
@@ -6,14 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  
+     
    
   constructor() { }
 
-  ngOnInit(): void {
-    
-
-  
+  ngOnInit():    void {
+   
   }
 
 }
--- src/assets/main.js
+++ src/assets/main.js
@@ -1,60 +1,49 @@
-
-//Menu de Barras com JS
-const btnMobile = document.getElementById('btn-mobile');
-
-function toggleMenu(event) {
-    if (event.type === 'touchstart') event.preventDefault()
-    const nav = document.getElementById('nav');
-    nav.classList.toggle('active');
-    const active = nav.classList.contains('active');
-    event.currentTarget.setAttribute('aria-expanded', active);
-    if (active) {
-        event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
-    } else {
-        event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+ document.onreadystatechange = x => {
+    //Menu de Barras com JS
+    const btnMobile = document.getElementById('btn-mobile');
+    
+    function toggleMenu(event) {
+        if (event.type === 'touchstart') event.preventDefault()
+        const nav = document.getElementById('nav');
+        nav.classList.toggle('active');
+        const active = nav.classList.contains('active');
+        event.currentTarget.setAttribute('aria-expanded', active);
+        if (active) {
+            event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+        } else {
+            event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+        }
     }
+    
+    btnMobile.addEventListener('click', toggleMenu);
+    btnMobile.addEventListener('touchstart', toggleMenu);
+    
+    //DOM- Document Object Model
+    // regex -expressoes regulares
 }
 
-
-btnMobile.addEventListener('click', toggleMenu);
-btnMobile.addEventListener('touchstart', toggleMenu);
-
-
-
-
-//contador carrinho
-     let numeroProdutos = 0
+function contarProdutos() {
+    //contador carrinho
+    let numeroProdutos = 0
     // numeroProdutos = 1
-   //  numeroProdutos = 1
+    //  numeroProdutos = 1
     let marcador = document.getElementById("marcador");
-
-   function contarProdutos() {
     numeroProdutos = numeroProdutos + 1
-     console.log(numeroProdutos)
+    console.log(numeroProdutos)
 
     marcador.innerText = numeroProdutos;
-   // marcador.innerHtml = numeroProdutos;
-   }
+    // marcador.innerHtml = numeroProdutos;
+}
 
-
-//email
-let email = document.getElementById("campo-email");
-let mensagem = document.getElementById("campo-mensagem");
 
 //window.alert("teste");
 function mostrarPopup() {
     window.alert("Hello World")
 }
 
-//DOM- Document Object Model
-// regex -expressoes regulares
-
 function enviarEmail() {
-    let emailDigitado = email.value;
-    console.log(emailDigitado)
-}
+    let email = document.getElementById("campo-email");
+    let mensagem = document.getElementById("campo-mensagem");
 
-function enviarMensagem() {
-    let mensagemDigitada = mensagem.value
-    console.log(mensagemDigitada)
+    console.log("e-mail: " + email.value + " | mensagem: " + mensagem.value)
 }
